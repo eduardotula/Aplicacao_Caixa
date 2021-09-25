15c33e2dbd9879ef0eba5cd520c041418b953705
Eduardo Muterle <eduardomuterle@outlook.com>
2021-09-25 19:00:56 -0300

    Primeiro Commit

---
+++ src/main/java/com/source/AplicacaoCaixaApplication.java
@@ -0,0 +1,13 @@
+package com.source;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacaoCaixaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacaoCaixaApplication.class, args);
+	}
+
+}
